13e8ed6a499325de35788d447b61250856877695
Arash_ST <31709401+ArashST79@users.noreply.github.com>
2023-06-30 15:28:45 +0330

    work in progress (private messaging)

--- client.py
+++ client.py
@@ -23,7 +23,6 @@ private_key = None
 public_key_bytes  = None
 private_key_bytes = None
 session_keys = {}
-dest_user = None
 dest_user_message = None
 dest_user_public_key = None
 diffie_private_key = None
@@ -319,6 +318,27 @@ def handle_public(data_header,data_main,server_socket):
     super_encryptes_message = encrypt_with_master_key(master_key,encrypted_message,header)
     server_socket.send(super_encryptes_message)
 
+def handle_forward(data_header,data_main,server_socket):
+    data_main = decrypt_data(master_key,data_main)
+    data_main_main , data_main_header = data_main.aplit()
+    if(data_main_header.startswith("REQUEST_SESSION")):
+        handle_create_session(data_main_header,data_main_main,server_socket)
+        encrypted_message = encrypt_for_public_key(data_main_header.split()[1])
+        server_socket.send(encrypted_message)
+
+def handle_create_session(data_header,data_main,server_socket):
+    data_header_parts = data_header.split("||")
+    from_user = data_header_parts[1]
+    data_main = decode_with_private_key(private_key,data_main).decode()
+    diffie_public_key_other = int(data_main)
+    global diffie_private_key, diffie_public_key
+    diffie_private_key = diffie_generate_private_key()
+    diffie_public_key = diffie_generate_public_key(diffie_private_key)
+    session_key = generate_session_key(diffie_private_key, diffie_public_key_other)
+    session_keys[from_user] = session_key
+
+    
+    
 # function to receive data from server
 def receive_data(server_socket):
     while True:
@@ -338,6 +358,8 @@ def receive_data(server_socket):
             if(data_header.startswith("PUBLIC")):
                 handle_public(data_header,data_main,server_socket)
 
+            if(data_header.startswith("FORWARD")):
+                handle_forward(data_header,data_main,server_socket)
 
 
 
